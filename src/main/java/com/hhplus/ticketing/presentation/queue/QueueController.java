52ad13d5ced3a5bd7feb94b67dc81de32e46580c
seoulJoy <nonofficial335@gmail.com>
2024-07-12 14:46:50 +0900

    fix: 대기열토큰 기능 및 단위테스트 수정

--- src/main/java/com/hhplus/ticketing/presentation/queue/QueueController.java
+++ src/main/java/com/hhplus/ticketing/presentation/queue/QueueController.java
@@ -1,11 +1,13 @@
 package com.hhplus.ticketing.presentation.queue;
 
+import com.hhplus.ticketing.application.queue.service.QueueService;
 import com.hhplus.ticketing.presentation.queue.dto.QueueRequestDto;
 import com.hhplus.ticketing.presentation.queue.dto.QueueResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "대기열 API")
@@ -14,26 +16,30 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class QueueController {
 
+    private final QueueService queueService;
+
     /**
-     * 대기열 요청
+     * 대기열 진입
      * @param requestDto
      * @return
      */
     @Operation(summary = "대기열 요청")
     @PostMapping("/")
-    public QueueResponseDto requestQueue(@RequestBody QueueRequestDto requestDto) {
-        return new QueueResponseDto("dG9rZW5WYWx1ZQ==",100, 3000);
+    public ResponseEntity<QueueResponseDto> requestQueue(@RequestBody QueueRequestDto requestDto) {
+        QueueResponseDto enterQueue = queueService.enterQueue(requestDto.getUserId());
+        return ResponseEntity.ok(enterQueue);
     }
 
     /**
      * 대기열 조회
-     * @param request
+     * @param authorization
      * @return
      */
     @Operation(summary = "대기열 조회")
     @GetMapping("/status")
-    public QueueResponseDto queueStatus(@RequestBody HttpServletRequest request) {
-        return new QueueResponseDto("dG9rZW5WYWx1ZQ==",100, 3000);
+    public ResponseEntity<QueueResponseDto> queueStatus(@RequestHeader("Authorization") String authorization) {
+        QueueResponseDto queueStatus = queueService.getQueueStatus(authorization);
+        return ResponseEntity.ok(queueStatus);
     }
 
 }
--- src/main/java/com/hhplus/ticketing/presentation/queue/dto/QueueRequestDto.java
+++ src/main/java/com/hhplus/ticketing/presentation/queue/dto/QueueRequestDto.java
@@ -1,8 +1,14 @@
 package com.hhplus.ticketing.presentation.queue.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@AllArgsConstructor
+@Builder
+@Data
 public class QueueRequestDto {
 
     private long userId;
-    private long concertId;
 
 }
